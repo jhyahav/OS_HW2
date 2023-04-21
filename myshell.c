3b2bf6b8df5dc63c0e6394d8f944a28b2027e9c4
Jonathan Yahav <jonathanhuck@mail.tau.ac.il>
2023-04-21 21:07:38 +0300

    redirect is the worst

--- myshell.c
+++ myshell.c
@@ -16,12 +16,13 @@
 #define SUCCESS 0
 #define NOT_FOUND -1
 
-void handle_background(int count, char **arglist);
+int handle_background(int count, char **arglist);
 int handle_pipe(int count, char **arglist);
 int handle_redirect(int count, char **arglist);
 int execute_pipe(int count, char **arglist, int pipe_index);
 int execute_redirect(int count, char **arglist);
 void execute_command(int count, char **arglist, int is_background);
+void clean_arglist(int count, char **arglist);
 
 int prepare(void)
 {
@@ -30,7 +31,6 @@ int prepare(void)
 
 int process_arglist(int count, char **arglist)
 {
-	handle_background(count, arglist);
 
 	int pipe_return = handle_pipe(count, arglist);
 	if (pipe_return != NOT_FOUND)
@@ -44,7 +44,10 @@ int process_arglist(int count, char **arglist)
 		return redirect_return;
 	}
 
-	execute_command(count, arglist, FALSE);
+	if (!handle_background(count, arglist))
+	{
+		execute_command(count, arglist, FALSE);
+	}
 
 	return 1;
 }
@@ -54,15 +57,17 @@ int finalize(void)
 	return SUCCESS;
 }
 
-void handle_background(int count, char **arglist)
+int handle_background(int count, char **arglist)
 {
 	if (arglist[count - 1][0] != RUN_IN_BACKGROUND)
 	{
-		return;
+		return FALSE;
 	}
 
 	arglist[count - 1] = NULL; // FIXME: prone to errors, make sure it's okay
+	printf("Background\n");
 	execute_command(count - 1, arglist, TRUE);
+	return TRUE;
 }
 
 int handle_pipe(int count, char **arglist)
@@ -104,25 +109,60 @@ int execute_pipe(int count, char **arglist, int pipe_index)
 
 int execute_redirect(int count, char **arglist)
 {
-	int fd = open(arglist[count - 1], O_RDONLY);
-	if (fd == NOT_FOUND)
+
+	pid_t pid = fork();
+	if (pid == NOT_FOUND)
 	{
 		perror(strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	if (dup2(fd, STDIN_FILENO) == NOT_FOUND)
+	else if (pid == 0)
 	{
-		perror(strerror(errno));
-		exit(EXIT_FAILURE);
+		// printf("Began execution: %s\n", arglist[0]);
+		int fd = open(arglist[count - 1], O_RDONLY);
+		// printf("Opened\n");
+		if (fd == NOT_FOUND)
+		{
+			perror(strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		if (dup2(fd, STDIN_FILENO) == NOT_FOUND)
+		{
+			perror(strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+
+		close(fd);
+
+		// int i = 0;
+		// while (*arglist != NULL && i < count)
+		// {
+		// 	printf(*(arglist + i++));
+
+		// }
+
+		// execvp(arglist[0], arglist); // FIXME: wrap with error handling
+	}
+	else
+	{
+
+		int status;
+		int pid = wait(&status);
+		if (WIFEXITED(status))
+		{
+			// printf("Child finished\n");
+		}
 	}
 
-	arglist[count - 2] = NULL;
-	execute_command(count - 2, arglist, FALSE);
+	// arglist[count - 2] = NULL;
+	// execute_command(count, arglist, FALSE);
+	// clean_arglist(count, arglist);
+	// execute_command(count - 1, arglist, FALSE);
 
 	return 1;
 }
 
-void execute_command(int count, char **arglist, int is_background) // could be int?
+void execute_command(int count, char **arglist, int is_background)
 {
 	if (count <= 0)
 	{
@@ -136,6 +176,7 @@ void execute_command(int count, char **arglist, int is_background) // could be i
 	}
 	else if (pid == 0)
 	{
+		// printf("Began execution: %s\n", arglist[0]);
 		execvp(arglist[0], arglist); // FIXME: wrap with error handling
 	}
 	else
@@ -147,7 +188,22 @@ void execute_command(int count, char **arglist, int is_background) // could be i
 			int pid = wait(&status);
 			if (WIFEXITED(status))
 			{
+				// printf("Child finished\n");
 			}
 		}
 	}
 }
+
+// void clean_arglist(int count, char **arglist)
+// {
+// 	int i = 0;
+// 	for (int j = 0; j < count; j++)
+// 	{
+// 		if (arglist[j][0] == REDIRECT)
+// 		{
+// 			continue;
+// 		}
+// 		printf(arglist[i]);
+// 		arglist[i++] = arglist[j];
+// 	}
+// }
